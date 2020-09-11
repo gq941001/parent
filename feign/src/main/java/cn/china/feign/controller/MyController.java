29f90b9f8470bd2ecf48e52d107e211a398e4bfc
gq941001 <gq19951005>
2020-09-11 15:45:56 +0800

    my

--- feign/src/main/java/cn/china/feign/controller/MyController.java
+++ feign/src/main/java/cn/china/feign/controller/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-    /*这是feign*/
+    /*这是feign的业务代码*/
     @Autowired
     MyInterface mi;
 
